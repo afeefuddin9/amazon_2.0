b802dde9ace12ea47a88d9c7b4215a435e3acdec
Mohammed Afeefuddin Patel <87899639+afeefuddin9@users.noreply.github.com>
2023-10-04 12:44:45 +0530

    Adding Amazon 2.0 code

---
+++ firebase.js
@@ -0,0 +1,14 @@
+var firebaseConfig = {
+  apiKey: "yoUr_API_kEy*******************JNqFe8",
+  authDomain: "yourapphere.firebaseapp.com",
+  projectId: "clone-project_Id",
+  storageBucket: "clone-project.appspot.com",
+  messagingSenderId: "58974425893",
+  appId: "1:58974425893:web:*******************JNqFe8",
+  measurementId: "G-ABGPP5876A",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+var db = firebase.firestore();
